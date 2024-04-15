db312f10387ed42907841a934be35111faa31a9c
swuY <swu341457@gmail.com>
2024-04-15 22:12:36 +0200

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from PIL import Image
+import numpy as np
+import math
+from scipy import signal
 import matplotlib.pyplot as plt
 
 
-def get_image_info(image_path: str) -> list[tuple]:
-    image = Image.open(image_path)
+def get_image_info(image_path: str, mode: str = "RGB") -> list[tuple]:
+    image = Image.open(image_path).convert(mode)
 
     pixel = image.load()  # 载入图片信息
 
@@ -13,8 +16,8 @@ def get_image_info(image_path: str) -> list[tuple]:
 
     info_list = []  # 创建空列表用于存储每个像素点的法向量
 
-    for x in range(width):
-        for y in range(height):
+    for y in range(height):
+        for x in range(width):
             info = pixel[x, y]
             info_list.append(info)
 
@@ -23,9 +26,17 @@ def get_image_info(image_path: str) -> list[tuple]:
 
 def main():
     original_image_colors = get_image_info("original_image.png")
-    normal_vector_list = get_image_info("normal_map.png")
+    normal_vector_list = get_image_info("normal_map.png", "L")
+
+    original_image = np.asarray(Image.open("original_image.png"))
+    map_image = np.asarray(Image.open("normal_map.png"))
+
+    print(original_image[0])
 
 
+
+    plt.imshow(original_image)
+    plt.show()
 
 
 if __name__ == '__main__':
